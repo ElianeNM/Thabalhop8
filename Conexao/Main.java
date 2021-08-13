97336d9ce577c9514b19b24144e01a38d4892a1b
Eliane Nunes <elianu.nun.mar@gmail.com>
2021-08-13 00:29:40 -0300

    commit

--- Conexao/Main.java
+++ Conexao/Main.java
@@ -29,15 +29,21 @@ public class Main{
         crud1.create(4,"tarde",1);
         
         //Consultando uma linha no banco 
-        //crud.read(1);
-        //crud1.read(2);
+        crud.read(1); //linha onde o Id_sala da tabela Sala é 1
+        crud1.read(2); //linha onde o Id_turma da tabela Turma é 2
       
         //Alterando um registro no banco
-        //crud.update(3,35);
+        crud.update(3,35); //atualizando capacidade da Sala com Id_sala 3 para 35(alunos)
         
         //Deletando um registro no banco
+        crud1.delete(2); //deletando o registro da tabela Turma onde Id_turma é 2
+        //crud1.delete(1);  
         //crud1.delete(2);
-    
+        //crud1.delete(3);
+        //crud1.delete(4);
+        //crud.delete(1);
+        //crud.delete(2);
+        //crud.delete(3);
         
         conexao1.fecharConexao();
         conexao.fecharConexao();     
